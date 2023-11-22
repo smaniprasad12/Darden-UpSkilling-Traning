b4147aac33d2056e3958fc62d7bc7efc750191c7
smaniprasad12 <104119726+smaniprasad12@users.noreply.github.com>
2023-11-22 11:16:37 +0530

    Delete Day-2/SpringBootJUnitTesting directory

--- Day-2/SpringBootJUnitTesting/src/main/java/com/example/demotestapplication/repo/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demotestapplication.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demotestapplication.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-
-}
